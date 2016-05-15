1d7d2d7e74c3775f2c429872bc53e9b67c8e88c0
Anders Kaseorg <andersk@mit.edu>
2016-05-15 19:07:41 -0400

    Add the empty function never : Never -> a
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- src/Basics/Extra.elm
+++ src/Basics/Extra.elm
@@ -1,6 +1,18 @@
-module Basics.Extra exposing (
+module Basics.Extra exposing
+  ( never
   )
 {-| Additional basic functions
 
-@docs
+@docs never
 -}
+
+{-| The empty function.
+This converts a value of type `Never` into a value of any type, which
+is safe because there are no values of type `Never`.
+-}
+never : Never -> a
+never n = never n
+-- If this function is moved to Basics, the following nicer definition
+-- using the private Never constructor may be used.  This actually
+-- witness the emptiness of Never instead of just asserting it:
+-- never (Never n) = never n
