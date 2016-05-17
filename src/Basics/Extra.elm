75a0b4a3b41013fc90fc2148762c58858a906876
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2016-05-17 06:43:14 +0200

    elaborate documentation

--- src/Basics/Extra.elm
+++ src/Basics/Extra.elm
@@ -7,12 +7,17 @@ module Basics.Extra exposing
 -}
 
 {-| The empty function.
-This converts a value of type `Never` into a value of any type, which
-is safe because there are no values of type `Never`.
+This converts a value of type
+[`Never`](http://package.elm-lang.org/packages/elm-lang/core/latest/Basics#Never)
+into a value of any type, which is safe because there are no values of
+type `Never`. Useful in certain situations as argument to
+[`Task.perform`](http://package.elm-lang.org/packages/elm-lang/core/latest/Task#perform),
+[`Cmd.map`](http://package.elm-lang.org/packages/elm-lang/core/latest/Platform-Cmd#map),
+[`Html.map`](http://package.elm-lang.org/packages/elm-lang/html/latest/Html-App#map).
 -}
 never : Never -> a
 never n = never n
 -- If this function is moved to Basics, the following nicer definition
 -- using the private Never constructor may be used.  This actually
--- witness the emptiness of Never instead of just asserting it:
+-- witnesses the emptiness of Never instead of just asserting it:
 -- never (Never n) = never n
