0e96186a7d18e8a175d208440c556122b34a792a
Dave Keen <dev@ruffness.com>
2016-05-25 22:17:53 +0200

    Added =>

--- src/Basics/Extra.elm
+++ src/Basics/Extra.elm
@@ -1,11 +1,13 @@
 module Basics.Extra
     exposing
         ( never
+        , (=>)
         )
 
 {-| Additional basic functions
 
 @docs never
+@docs =>
 -}
 
 
@@ -33,3 +35,10 @@ never n =
 --
 -- never (Never n) =
 --     never n
+
+
+{-| A shorthand for writing 2-tuples.  Very commonly used when expressing key/value pairs
+in CSS or Json encoders.
+-}
+(=>) : a -> b -> (a, b)
+(=>) = (,)
