af60cec946a60e47c074e7afc153eaab9325f7d9
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2016-05-26 11:28:09 +0200

    Docu improvement for never (#2)
    * properly point to potential uses
    
    * add an example
    
    * bump package version number

--- src/Basics/Extra.elm
+++ src/Basics/Extra.elm
@@ -14,9 +14,15 @@ This converts a value of type
 [`Never`](http://package.elm-lang.org/packages/elm-lang/core/latest/Basics#Never)
 into a value of any type, which is safe because there are no values of
 type `Never`. Useful in certain situations as argument to
-[`Task.perform`](http://package.elm-lang.org/packages/elm-lang/core/latest/Task#perform),
-[`Cmd.map`](http://package.elm-lang.org/packages/elm-lang/core/latest/Platform-Cmd#map),
-[`Html.map`](http://package.elm-lang.org/packages/elm-lang/html/latest/Html-App#map).
+[`Task.perform`](http://package.elm-lang.org/packages/elm-lang/core/latest/Task#perform)
+and
+[`Html.App.map`](http://package.elm-lang.org/packages/elm-lang/html/latest/Html-App#map).
+
+*Note:* To use this function, its argument need not be literally of type `Never`.
+It suffices if it is a fully polymorphic value. For example, this works:
+`Process.sleep >> Task.perform never (\() -> ...)`, because the output of
+[`Process.sleep`](http://package.elm-lang.org/packages/elm-lang/core/latest/Process#sleep)
+is fully polymorphic in the `x` of `Task x ()`. 
 -}
 never : Never -> a
 never n =
