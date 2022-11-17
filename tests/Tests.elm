33115d63c9b24e704384bc4c06aec707e8fee12e
John Pavlick <jmpavlick@gmail.com>
2022-11-16 21:18:43 -0500

    master: coverage is coveraged

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,6 +1,28 @@
 module Tests exposing (suite)
 
-import Basics.Extra exposing (fractionalModBy, inRadians, inTurns, orderBy, toOrder)
+import Basics.Extra
+    exposing
+        ( atLeast
+        , atMost
+        , curry
+        , flip
+        , fractionalModBy
+        , inDegrees
+        , inRadians
+        , inTurns
+        , isSafeInteger
+        , maxSafeInteger
+        , minSafeInteger
+        , orderBy
+        , safeDivide
+        , safeIntegerDivide
+        , safeModBy
+        , safeRemainderBy
+        , swap
+        , toOrder
+        , toOrderDesc
+        , uncurry
+        )
 import Expect exposing (Expectation, FloatingPointTolerance(..))
 import List.Extra as ListX
 import Test exposing (Test, describe, test)
@@ -9,17 +31,121 @@ import Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "Basics.Extra additional doc tests"
-        [ fractionalModByDocTests
+        [ swapTest
+        , numbersTests
+        , fractionalModByDocTests
+        , inDegreesTest
         , inRadiansDocTests
         , inTurnsDocTests
+        , higherOrderHelpersTests
         , orderByTests
         , toOrderTests
+        , toOrderDescTests
         ]
 
 
 expectAlmostEqual : Float -> Float -> Expectation
 expectAlmostEqual =
     Expect.within (Absolute 1.0e-20)
+
+
+swapTest : Test
+swapTest =
+    test "swap swaps" <|
+        \() ->
+            swap ( 2, 0 ) |> swap |> Expect.equal ( 2, 0 )
+
+
+numbersTests : Test
+numbersTests =
+    describe "numbers tests"
+        [ test "maxSafeInteger behaves unexpectedly, as expected" <|
+            \() ->
+                maxSafeInteger + 1 |> Expect.equal (maxSafeInteger + 2)
+        , test "minSafeInteger behaves unexpectedly, as expected" <|
+            \() ->
+                minSafeInteger - 1 |> Expect.equal (minSafeInteger - 2)
+        , test "reports if an integer is within the range of safety" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        isSafeInteger 5 |> Expect.equal True
+                    , \() ->
+                        isSafeInteger maxSafeInteger |> Expect.equal True
+                    , \() ->
+                        isSafeInteger minSafeInteger |> Expect.equal True
+                    , \() ->
+                        minSafeInteger - 1 |> isSafeInteger |> Expect.equal False
+                    , \() ->
+                        maxSafeInteger + 1 |> isSafeInteger |> Expect.equal False
+                    ]
+                    ()
+        , test "defines an upper bound for a variable" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        42 |> atMost 0 |> Expect.equal 0
+                    , \() ->
+                        -42 |> atMost 0 |> Expect.equal -42
+                    ]
+                    ()
+        , test "defines a lower bound for a variable" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        -42 |> atLeast 0 |> Expect.equal 0
+                    , \() ->
+                        42 |> atLeast 0 |> Expect.equal 42
+                    ]
+                    ()
+        , test "divides a floating point number safely" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        safeDivide 5 2 |> Expect.equal (Just 2.5)
+                    , \() ->
+                        safeDivide 5 0 |> Expect.equal Nothing
+                    ]
+                    ()
+        , test "divides an integer safely" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        safeIntegerDivide 5 2 |> Expect.equal (Just 2)
+                    , \() ->
+                        safeIntegerDivide 5 0 |> Expect.equal Nothing
+                    ]
+                    ()
+        , test "all of the modular arithmetic and none of the crashing" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        safeModBy 2 4 |> Expect.equal (Just 0)
+                    , \() ->
+                        safeModBy 2 5 |> Expect.equal (Just 1)
+                    , \() ->
+                        safeModBy 0 4 |> Expect.equal Nothing
+                    ]
+                    ()
+        , test "safe remainders and none of the crashing" <|
+            \() ->
+                Expect.all
+                    [ \() ->
+                        safeRemainderBy 2 4 |> Expect.equal (Just 0)
+                    , \() ->
+                        safeRemainderBy 2 5 |> Expect.equal (Just 1)
+                    , \() ->
+                        safeRemainderBy 0 4 |> Expect.equal Nothing
+                    ]
+                    ()
+        ]
+
+
+inDegreesTest : Test
+inDegreesTest =
+    test "inDegrees should behave as expected" <|
+        \() ->
+            inDegrees pi |> Expect.equal 180
 
 
 fractionalModByDocTests : Test
@@ -51,6 +177,21 @@ inTurnsDocTests =
             \() -> inTurns (degrees 180) |> expectAlmostEqual 0.5
         , test "example 2" <|
             \() -> inTurns (3 * pi) |> expectAlmostEqual 1.5
+        ]
+
+
+higherOrderHelpersTests : Test
+higherOrderHelpersTests =
+    describe "higher-order helpers"
+        [ test "flip" <|
+            \() ->
+                flip safeDivide 5 0 |> Expect.equal (Just 0)
+        , test "curry" <|
+            \() ->
+                curry identity 2 4 |> Expect.equal ( 2, 4 )
+        , test "uncurry" <|
+            \() ->
+                uncurry safeDivide ( 0, 5 ) |> Expect.equal (Just 0)
         ]
 
 
@@ -258,4 +399,14 @@ toOrderTests =
                         [ A, A, B, B, B ]
                 in
                 List.sortWith order unsorted |> Expect.equalLists sorted
+        ]
+
+
+toOrderDescTests : Test
+toOrderDescTests =
+    describe "toOrderDesc"
+        [ test "verify that sorting in descending order works as expected" <|
+            \() ->
+                List.sortWith (toOrderDesc identity) [ 1, 2, 1, 3, 4 ]
+                    |> Expect.equalLists [ 4, 3, 2, 1, 1 ]
         ]
